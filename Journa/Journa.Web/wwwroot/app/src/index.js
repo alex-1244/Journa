09ebb086a60351df698d70c2e42d99795821dd8b
AlexTertyshnyk <alex112244@gmail.com>
2020-06-08 12:43:27 +0300

    slight improvements for react loading time

--- Journa/Journa.Web/wwwroot/app/src/index.js
+++ Journa/Journa.Web/wwwroot/app/src/index.js
@@ -1,7 +1,6 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-//import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
